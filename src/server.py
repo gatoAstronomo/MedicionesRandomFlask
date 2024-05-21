3f4540782a43a19572185ac6530ca10be872cf35
Arturo Rivas <a.rivas06@ufromail.cl>
2024-05-21 19:45:01 -0300

    First Commit

---
+++ src/server.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+
+FLASK_PORT = 8081
+MONGO_URL = "mongodb://localhost:27017/pythonmongodb"
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = MONGO_URL
+mongo = PyMongo(app)
