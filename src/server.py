111136a36747c884ac123e944ad893756093773c
Arturo Rivas <a.rivas06@ufromail.cl>
2024-05-22 00:37:22 -0300

    Imprime las mediciones que llegan al servidor

--- src/server.py
+++ src/server.py
@@ -14,7 +14,7 @@ def post():
         device_name = medicion["device_name"]
         epoch_time = medicion["epoch_time"]
         value = medicion["value"]
-        print(request.data())
+        print(dict(request.get_json()))
 
         # Buscamos un documento con el mismo nombre de dispositivo
         device = mongo.db.mediciones.find_one({"device_name": device_name})
