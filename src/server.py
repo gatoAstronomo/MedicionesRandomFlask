2d2c28627ced03c38837c3f3df1447dea3fa50bd
Arturo Rivas <a.rivas06@ufromail.cl>
2024-05-21 21:33:00 -0300

    agrega funcion post para agregar una medición a la coleccion mediciones de mongodb

--- src/server.py
+++ src/server.py
@@ -1,11 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-
-FLASK_PORT = 8081
-MONGO_URL = "mongodb://localhost:27017/pythonmongodb"
+from bson.json_util import dumps
+import json
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = MONGO_URL
+app.config["MONGO_URI"] = "mongodb://localhost:27017/005"
 mongo = PyMongo(app)
 
-print("probando el servidor")
+@app.route('/api/mediciones', methods=['POST'])
+def post():
+    if request.is_json:
+        medicion = request.get_json()
+        device_name = medicion["device_name"]
+        timestamp = medicion["timestamp"]
+        value = medicion["value"]
+
+        # Buscamos un documento con el mismo nombre de dispositivo
+        device = mongo.db.mediciones.find_one({"device_name": device_name})
+
+        if device:
+            # Si se encuentra un documento con el mismo nombre de dispositivo,
+            # agregamos la nueva medición a la lista de mediciones existente
+            mongo.db.mediciones.update_one(
+                {"_id": device["_id"]},
+                {"$push": {"mediciones": {"timestamp": timestamp, "value": value}}}
+            )
+
+            return jsonify({"message": "Medición agregada correctamente"}), 201
+        else:
+            # Si no se encuentra un documento con el mismo nombre de dispositivo,
+            # creamos un nuevo documento con el nombre de dispositivo y la lista de mediciones
+            mongo.db.mediciones.insert_one({
+                "device_name": device_name,
+                "mediciones": [{"timestamp": timestamp, "value": value}]
+            })
+
+            return jsonify({"message": "Medición agregada correctamente"}), 201
+    else:
+        return jsonify({"message": "Solicitud no válida"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8081)
