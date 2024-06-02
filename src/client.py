9e549f7e484179e223fe47cbc17d727a730659ed
Arturo Rivas <a.rivas06@ufromail.cl>
2024-06-02 20:28:08 -0300

    El grafico funciona perfectamente

--- src/client.py
+++ src/client.py
@@ -9,10 +9,13 @@ import matplotlib.animation as animation
 def pedirUltimaMedicion(device_name):
     try:
         response = requests.get(f'http://44.197.32.169:8081/mediciones?device_name={device_name}')
-        return response.json()
+        medicion = response.json()
+        medicion["epoch_time"] += 3600 * 3
+        return medicion
     except:
         print("no fue posible establecer conexión")
         return {}
+
 
 def devolverDatos(medicion):
     epoch_time = medicion.get("epoch_time")
@@ -28,17 +31,27 @@ def imprimirMedicion(medicion):
     epoch_time, temperature = devolverDatos(medicion)
     print(f'{formatEpochTime(epoch_time)} {temperature}ºC')
 
+def transformarEpochTime(medicion):
+    return mdates.date2num(datetime.datetime.fromtimestamp(medicion.get("epoch_time")))
+
 def graficarMediciones(mediciones):
-    epoch_times = [medicion.get("epoch_time") for medicion in mediciones]
+    # Convertir los valores de epoch_time a un formato numérico que se pueda utilizar en el eje de las X
+    epoch_times = [transformarEpochTime(medicion) for medicion in mediciones]
     temperatures = [medicion.get("temperature") for medicion in mediciones]
 
     fig, ax = plt.subplots()
     ax.xaxis_date()
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
 
     line, = ax.plot(epoch_times, temperatures)
-    plt.ylim(min(temperatures) - 2, max(temperatures) + 2)
-    plt.xlim(epoch_times[-len(epoch_times)//60], epoch_times[-1])
+    plt.ylim(min(temperatures) - 0.5, max(temperatures) + 0.5)
+
+    ultima_medicion = mediciones[-1]
+    primera_medicion = mediciones[0]
+    limite_superior = transformarEpochTime(ultima_medicion)
+    limite_inferior = transformarEpochTime(primera_medicion)
+    ax.set_xlim(limite_inferior, limite_superior)
+
     plt.title('Temperatura en tiempo real')
     plt.xlabel('Tiempo')
     plt.ylabel('Temperatura (ºC)')
@@ -46,34 +59,42 @@ def graficarMediciones(mediciones):
     def actualizar(i):
         try:
             ultima_medicion = pedirUltimaMedicion("Nodo005")
+            epoch_times.append(transformarEpochTime(ultima_medicion))
+            temperatures.append(ultima_medicion.get("temperature"))
             if ultima_medicion.get("epoch_time") != mediciones[-1].get("epoch_time"):
                 mediciones.append(ultima_medicion)
-                line.set_xdata([medicion.get("epoch_time") for medicion in mediciones])
+                line.set_xdata([transformarEpochTime(medicion) for medicion in mediciones])
                 line.set_ydata([medicion.get("temperature") for medicion in mediciones])
                 ax.relim()
                 ax.autoscale_view()
-                plt.ylim(min(temperatures) - 2, max(temperatures) + 2)
-                plt.xlim(epoch_times[-len(epoch_times)//60], epoch_times[-1])
+                plt.ylim(min(temperatures) - 0.5, max(temperatures) + 0.5)
+
+                ultima_medicion = mediciones[-1]
+                primera_medicion = mediciones[0]
+                limite_superior = transformarEpochTime(ultima_medicion)
+                limite_inferior = transformarEpochTime(primera_medicion)
+                ax.set_xlim(limite_inferior, limite_superior)
+                
+                ax.figure.canvas.draw()
         except Exception as e:
             print(f"Error al actualizar el gráfico: {e}")
 
-    ani = animation.FuncAnimation(fig, actualizar, interval=1000)
+    ani = animation.FuncAnimation(fig, actualizar, interval=100)
     plt.show()
-
 
 mediciones = []
 mediciones.append(pedirUltimaMedicion("Nodo005"))
 
-def mainloop():
-    # graficarMediciones(mediciones)
+def main():
+    graficarMediciones(mediciones)
+    """
     while True:
-        medicion = pedirUltimaMedicion("Nodo005")
+         medicion = pedirUltimaMedicion("Nodo005")
         ultimaMedicion = mediciones[-1]
         if medicion.get("epoch_time") != ultimaMedicion.get("epoch_time"):
-            imprimirMedicion(medicion)
             mediciones.append(medicion)
-        
-
-
+            imprimirMedicion(medicion)
+    """
+            
 if __name__ == "__main__":
-    mainloop()
+    main()
