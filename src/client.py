222f55c688706c47ca74fd8b3f589e3ccbee0842
Arturo Rivas <a.rivas06@ufromail.cl>
2024-06-02 18:16:04 -0300

    Cambia el formato del json que contiene la medicion, value => temperature

--- src/client.py
+++ src/client.py
@@ -16,8 +16,8 @@ def pedirUltimaMedicion(device_name):
 
 def devolverDatos(medicion):
     epoch_time = medicion.get("epoch_time")
-    value = medicion.get("value")
-    return epoch_time, value
+    temperature = medicion.get("temperature")
+    return epoch_time, temperature
 
 def formatEpochTime(epoch_time):
     dt_object = datetime.datetime.fromtimestamp(epoch_time)
@@ -25,19 +25,19 @@ def formatEpochTime(epoch_time):
     return formatted_date
 
 def imprimirMedicion(medicion):
-    epoch_time, value = devolverDatos(medicion)
-    print(f'{formatEpochTime(epoch_time)} {value}ºC')
+    epoch_time, temperature = devolverDatos(medicion)
+    print(f'{formatEpochTime(epoch_time)} {temperature}ºC')
 
 def graficarMediciones(mediciones):
     epoch_times = [medicion.get("epoch_time") for medicion in mediciones]
-    values = [medicion.get("value") for medicion in mediciones]
+    temperatures = [medicion.get("temperature") for medicion in mediciones]
 
     fig, ax = plt.subplots()
     ax.xaxis_date()
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
 
-    line, = ax.plot(epoch_times, values)
-    plt.ylim(min(values) - 2, max(values) + 2)
+    line, = ax.plot(epoch_times, temperatures)
+    plt.ylim(min(temperatures) - 2, max(temperatures) + 2)
     plt.xlim(epoch_times[-len(epoch_times)//60], epoch_times[-1])
     plt.title('Temperatura en tiempo real')
     plt.xlabel('Tiempo')
@@ -49,10 +49,10 @@ def graficarMediciones(mediciones):
             if ultima_medicion.get("epoch_time") != mediciones[-1].get("epoch_time"):
                 mediciones.append(ultima_medicion)
                 line.set_xdata([medicion.get("epoch_time") for medicion in mediciones])
-                line.set_ydata([medicion.get("value") for medicion in mediciones])
+                line.set_ydata([medicion.get("temperature") for medicion in mediciones])
                 ax.relim()
                 ax.autoscale_view()
-                plt.ylim(min(values) - 2, max(values) + 2)
+                plt.ylim(min(temperatures) - 2, max(temperatures) + 2)
                 plt.xlim(epoch_times[-len(epoch_times)//60], epoch_times[-1])
         except Exception as e:
             print(f"Error al actualizar el gráfico: {e}")
